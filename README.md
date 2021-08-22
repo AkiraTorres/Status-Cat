a5dddfc4ac08a2d225a782576bb70ca38b6ef9ec
AkiraTorres <torres.thg@gmail.com>
2021-08-22 05:06:08 -0300

    basic commands and project's structure created

---
+++ README.md
@@ -0,0 +1,1 @@
+# HTTP Cats Bot v2
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import TelegramBot from "node-telegram-bot-api";
+import statusCode from "./statusCode.json";
+
+const TOKEN = "917350378:AAHJF69eFzjWACjhVErLP0p8rx1-GYjXFRY";
+const Cats = "https://http.cat/"
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on("new_chat_members", (msg) => {
+    bot.sendMessage(
+        msg.chat.id, 
+        `Olá ${msg.from.first_name}, digite /help para ter acesso a uma lista de comandos.`);
+});
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const response = `Comandos do bot:\n/echo [argumento]: Irá enviar uma mensagem repetindo o argumento enviado.
+/http [code]: Envia uma imagem e um leve resumo do código HTTP passado como argumento.`;
+''
+    bot.sendMessage(chatId, response);
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const response = match[1];
+
+    bot.sendMessage(chatId, response);
+});
+
+bot.onText(/\/http (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const http = match[1];
+    const url = Cats + match[1];
+    const caption = `Code ${statusCode[http].code} - ${statusCode[http].phrase}:\n${statusCode[http].description}`;
+
+    bot.sendPhoto(chatId, url, {caption: caption});
+});
